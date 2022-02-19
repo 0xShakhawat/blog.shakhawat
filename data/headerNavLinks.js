e61660a43612dc02134511f0af226f7b5b004636
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-19 17:12:04 +0600

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://www.youtube.com/shakhawat', title: 'YouTube' },
 ]
 
 export default headerNavLinks
