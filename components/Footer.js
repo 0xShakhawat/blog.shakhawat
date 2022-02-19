d9a905e61a30fc16aa42f9f17b1f17162cf2d33c
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-19 18:56:30 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -15,15 +15,15 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <Link herf="https://shakhawat.me">{siteMetadata.author}</Link>
+          <Link herf="https://www.shakhawat.me/">{siteMetadata.author}</Link>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://github.com/0xShakhawat/blog.shakhawat">
+            View GitHub Repository
           </Link>
         </div>
       </div>
