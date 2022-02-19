b0fe16964b75b57a2f870bc0230691e9ddd38033
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-19 17:17:12 +0600

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Shakhawat's Blog',
-  author: 'Tails Azimuth',
+  title: 'Shakhawat Blog',
+  author: 'Shakhawat',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A personal blog for Shakhawat',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
